6251fcad23fae11af9053c5ce43292ad4b036607
eternity <45088542+EternalRift@users.noreply.github.com>
2021-04-02 02:01:18 -0700

    Update utils.hpp

--- scout/utils.hpp
+++ scout/utils.hpp
@@ -11,7 +11,7 @@
 
 #include "web.hpp"
 
-// website title  website url
+												 // website title  website url
 using website_t = std::tuple<std::string, std::string>;
 
 const std::string logo = R"(                       _     _   ___  
@@ -29,25 +29,29 @@ namespace cmd
 {
 	auto print_logo() -> void;
 
-	auto output_request(const website_t& website, 
+	auto output_request(
+		const website_t& website, 
 		const int status_code) -> void;
 
 	auto initialize() -> void;
 
-	auto parameter_exists(char** begin, char** end, const std::string& option) -> bool;
+	auto parameter_exists(
+		char** begin, 
+		char** end, 
+		const std::string& option) -> bool;
 
   auto check_parameters(int argc, char* argv[]) -> bool;
-
 	auto set_console_cursor(bool shown) -> void;
 
-	inline const char* white = "\033[0m";
-	inline const char* black = "\x1B[30m";
-	inline const char* green = "\x1B[32m";
+	// console colors (portable)
+	inline const char* white  = "\033[0m";
+	inline const char* black  = "\x1B[30m";
+	inline const char* green  = "\x1B[32m";
 	inline const char* orange = "\x1B[33m";
-	inline const char* blue = "\x1B[36;1m";
+	inline const char* blue   = "\x1B[36;1m";
 	inline const char* purple = "\033[35m";
-	inline const char* clear = "\033c";
-	inline const char* red = "\x1B[31m";
+	inline const char* clear  = "\033c";
+	inline const char* red    = "\x1B[31m";
 
 	inline std::shared_timed_mutex m{};
 }
